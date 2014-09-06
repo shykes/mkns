b0acc48dea2f87c52161974e48e907ea9ab90d78
Solomon Hykes <solomon@docker.com>
2014-09-06 06:07:04 +0000

    Create persistent Linux namespaces from the command line
    Signed-off-by: Solomon Hykes <solomon@docker.com>

---
+++ mkns.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s net|pid|mnt PATH\n", os.Args[0])
+		os.Exit(1)
+	}
+	if err := mkns(os.Args[1], os.Args[2]); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(os.Args[2])
+}
+
+func mkns(nsType, mntPath string) error {
+	flags := map[string]int{
+		"net": syscall.CLONE_NEWNET,
+		"mnt": syscall.CLONE_NEWNS,
+		"pid": syscall.CLONE_NEWPID,
+		"ipc": syscall.CLONE_NEWIPC,
+	}
+	flag, ok := flags[nsType]
+	if !ok {
+		return fmt.Errorf("unsupported namespace type: %s", nsType)
+	}
+	if err := syscall.Unshare(flag); err != nil {
+		return err
+	}
+	dir, _ := path.Split(mntPath)
+	_ = os.MkdirAll(dir, 0700)
+	if err := syscall.Mount("/proc/self/ns/net", mntPath, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		return err
+	}
+	return nil
+}
